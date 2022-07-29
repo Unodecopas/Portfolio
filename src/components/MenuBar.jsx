827eced06f9e0f248896954bbc2a330f4f85c41e
Jesus <gallardomoralesjesus@gmail.com>
2022-07-29 20:39:51 +0200

    feat: MenuBar && text in readme

---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MenuBar = () => {
+  return (
+    <div className='menu-bar'>
+        <ul>
+            <li>File</li>
+            <li>Edit</li>
+            <li>Help</li>
+        </ul>
+    </div>
+  )
+}
+
+export default MenuBar
--- src/components/Readme.jsx
+++ src/components/Readme.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import MenuBar from './MenuBar'
 import WindowsBar from './WindowsBar'
 
 const Readme = () => {
   return (
     <div className='notepad'>
-        <WindowsBar title='Block de Notas - README.txt'/>
+        <WindowsBar title='Block de Notas - README.txt (Solo lectura)'/>
+        <MenuBar />
+        <div className="content">
+            <p>!Bienvenido a mi PORTFOLIO!</p>
+            <p>En esta pequeña máquina virtual he introducido todos los proyectos de React que he construido</p>
+            <p>¡Incluso el propio portofolio está realizado con REACT!</p>
+            <p>Te invito a que investigues un poco en este mini pc e intentes descubrir todos los proyectos que hay!!!</p>
+            <p>Te dejo una pequeña lista, por si no te das cuenta de alguno:</p>
+            <ul>
+                <li> - Clock</li>
+            </ul>
+            <p>AVISO!!! El portfolio aún está en desarrollo, si ves que algo no funciona como debería probablemente esté en desarrollo</p>
+        </div>
     </div>
   )
 }
