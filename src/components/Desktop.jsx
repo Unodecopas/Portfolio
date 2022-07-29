4ad2aa5b7da624418d549cdad2405468bcc2f622
Jesus <gallardomoralesjesus@gmail.com>
2022-07-29 20:01:55 +0200

    feat: linkedin Icon now link to linkedin

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -5,23 +5,25 @@ const Desktop = () => {
     <div className="desktop">
        <article>
         <a href="https:github.com/unodecopas" target='blank'>
-        <div className='githubIcon'></div>
-        <p>Github</p>
+          <div className='githubIcon'></div>
+          <p>Github</p>
         </a>
        </article>
        <article>
         <a href={cv}>
-        <div className='pdfIcon'></div>
-        <p>CV.pdf</p>
+          <div className='pdfIcon'></div>
+          <p>CV.pdf</p>
         </a>
        </article>
        <article>
-        <div className='notepadIcon'></div>
-        <p>Readme.txt</p>
+          <div className='notepadIcon'></div>
+          <p>Readme.txt</p>
        </article>
        <article>
-        <div className='linkedinIcon'></div>
-        <p>Linkedin</p>
+        <a href="https://linkedin.com/in/jegallardo" target='blank'>
+          <div className='linkedinIcon'></div>
+          <p>Linkedin</p>
+        </a>
        </article>
     </div>
   )
