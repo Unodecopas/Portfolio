75bb2461d1b6b5b99451baaed193a2c7229ab74c
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-07-29 17:50:20 +0200

    style: Desktop

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 const Desktop = () => {
   return (
-    <div className="desktop">Desktop</div>
+    <div className="desktop">
+        <article>
+            asd
+        </article>
+        <article>
+            asd
+        </article><article>
+            asd
+        </article><article>
+            asd
+        </article><article>
+            asd
+        </article><article>
+            asd
+        </article><article>
+            asd
+        </article>
+    </div>
   )
 }
 export default Desktop
